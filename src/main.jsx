e5342e679b8b6c74f13d10b96259bc78c1d692d2
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-09-05 22:12:59 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
+    authorizationParams.redirect_uri: window.location.origin
   >
     <Router>
       <App />
