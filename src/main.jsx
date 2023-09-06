98c0ca0322bd947a2d3c1dcbd53571d851707209
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-09-05 22:19:27 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,16 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-const domain = import.meta.env.REACT_APP_AUTH0_DOMAIN
-const clientId = import.meta.env.REACT_APP_AUTH0_CLIENT_ID
+const domain = import.meta.env.VITE_AUTH0_DOMAIN
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    authorizationParams.redirect_uri: "https://web54-kb-v2.vercel.app"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
   >
     <Router>
       <App />
