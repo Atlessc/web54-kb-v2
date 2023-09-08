6d0a51de6c39f2409d2d42e1d67d393b84b1ef5a
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-09-07 22:15:05 -0700

    Update top-nav.jsx

--- src/components/top-nav.jsx
+++ src/components/top-nav.jsx
@@ -4,7 +4,7 @@ export default function TopNav() {
   return (
     <div className="top-nav-container">
       <div className='company-name'><span className='orange'>Web54</span> KB</div>
-
+      /*when adding the search bar, make sure its a ternary operator with "!isAuthenticated"*/
     </div>
   )
 }
