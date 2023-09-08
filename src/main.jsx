181d913c33859f96e5c6b8992357b94625412144
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-09-07 22:21:25 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,17 @@ const domain = import.meta.env.VITE_AUTH0_DOMAIN
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{
-      redirect_uri: {window.location.origin}
-    }}
-  >
-    <Router>
-      <App />
-    </Router>
-  </Auth0Provider>
+  <React.StrictMode>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: {window.location.origin}
+      }}
+    >
+      <Router>
+        <App />
+      </Router>
+    </Auth0Provider>
+  </React.StrictMode>
 )
