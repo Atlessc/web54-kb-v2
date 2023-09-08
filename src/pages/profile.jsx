d42f9253269f5d674dec4c8e48fa1134c7b21997
Tyler Smith <tylersmith4596@gmail.com>
2023-09-08 10:08:04 -0700

    miss typed "isAuthenticated" in the profile file

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Profile() {
-  const { user, isAuthenticatted } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   return (
     isAuthenticated && (
       <div className="user-profile-container">
