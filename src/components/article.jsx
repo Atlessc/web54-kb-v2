4f9df27734182c3d1124cc10dbf60fa861a19889
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-09-08 23:06:54 -0700

    Update article.jsx

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -3,24 +3,53 @@ import ReactMarkdown from 'react-markdown';
 import { useState, useEffect } from 'react';
 import '../styles/Article.css';
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Sorry, something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
 function Article() {
   const { id } = useParams();
-  console.trace();
   const [markdown, setMarkdown] = useState('');
 
   useEffect(() => {
-    const path = `public/${id}.md`;
-    console.log(`Fetching markdown file from path: ${path}`);
+    const path = `/public/${id}.md`;
+    console.log(`Fetching markdown file from absolute path: ${window.location.origin}${path}`);
     fetch(path)
-      .then((response) => response.text())
-      .then((text) => setMarkdown(text));
+      .then((response) => {
+        if (!response.ok) { throw response }
+        return response.text()
+      })
+      .then((text) => setMarkdown(text))
+      .catch((error) => console.log(`There was an error: ${error}`));
   }, [id]);
 
   return (
-    <div>
+    <ErrorBoundary>
+      <div>
         <h1>yes its working</h1>
-<ReactMarkdown className='markdown'>{markdown}</ReactMarkdown>;
-</div>
-)}
+        <ReactMarkdown className='markdown'>{markdown}</ReactMarkdown>
+      </div>
+    </ErrorBoundary>
+  );
+}
 
 export default Article;
